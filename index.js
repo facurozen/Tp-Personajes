89456d931c0e026296ba0862dcdf0a75b6411058
SANTINO ARANA <47031761@ort>
2023-05-12 11:30:21 -0300

    Crud e index

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ app.put('/Pelicula',async(req,res)=>{
         res.status(200).json({message:'Pelicula actualizada'});
     }   catch (error){
         console.error(error);
-        res.status(500).json({error:'Fallo el update'});
+        res.status(500).json({error:'Fallo el update'});  
+        
     }
 })
 
